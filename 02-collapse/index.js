6a571f4b27e529a422edde5abb3e364c6b448e04
Eduard Simonenko <edik.simonenko2019@gmail.com>
2022-08-31 23:21:50 +0300

    feat: add 02-collapse solution

--- 02-collapse/index.js
+++ 02-collapse/index.js
@@ -1,1 +1,60 @@
-/* Я комментарий, который ждал тебя здесь. Удали меня и напиши хороший код! */
+const collapsibleButton = document.querySelector('.collapsible__button');
+const collapsibleContent = document.querySelector('.collapsible__content');
+const collapsibleActionHidden = document.querySelector('.collapsible__action--hidden');
+const collapsibleActionVisible = document.querySelector('.collapsible__action--visible');
+
+const contentEmergence = [
+  { visibility: 'visible' },
+  { position: 'static' },
+];
+const contentHiding = [
+  { visibility: 'hidden' },
+  { position: 'absolute', top: '-100%'},
+];
+
+const collapsAnimationTiming = { fill: 'forwards' }
+
+
+function showContent() {
+  collapsibleContent.animate(
+    contentHiding,
+    collapsAnimationTiming,
+  )
+  collapsibleActionVisible.animate(
+    contentHiding,
+    collapsAnimationTiming,
+  )
+  collapsibleActionHidden.animate(
+    contentEmergence,
+    collapsAnimationTiming,
+  )
+}
+
+function hideContent() {
+  collapsibleContent.animate(
+    contentEmergence,
+    collapsAnimationTiming,
+  )
+  collapsibleActionVisible.animate(
+    contentEmergence,
+    collapsAnimationTiming,
+  )
+  collapsibleActionHidden.animate(
+    contentHiding,
+    collapsAnimationTiming,
+  )
+}
+
+collapsibleActionHidden.animate(
+  contentHiding,
+  collapsAnimationTiming,
+)
+
+collapsibleButton.addEventListener('click', (event) => {
+  const eventClassName = event.target.className;
+  const isHideButton = eventClassName.includes('collapsible__action--visible');
+  isHideButton ? showContent() : hideContent()
+})
+
+
+
